d4235315bff91de9478689f47a2c54e25dbe9da2
nehadeshpande89 <nehadeshpande1995@gmail.com>
2020-12-12 14:08:17 +0000

    changes

--- includes/conn.php
+++ includes/conn.php
@@ -2,9 +2,9 @@
 
 Class Database{
  
-	private $server = "mysql:host=localhost;dbname=ecomm";
-	private $username = "root";
-	private $password = "";
+	private $server = "mecomm.cp0j8kjw11ud.us-east-1.rds.amazonaws.com";
+	private $username = "neha";
+	private $password = "12345678";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
 	protected $conn;
  	
