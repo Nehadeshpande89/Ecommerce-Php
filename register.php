8d4551a84c533c7d7470581e84de4930890e5fc6
nehadeshpande89 <nehadeshpande1995@gmail.com>
2020-12-19 16:59:48 +0000

    changes

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
 
 	include 'includes/session.php';
 	include 'includes/Email_library.php';
-
+  
 
 	if(isset($_POST['signup'])){
 		$firstname = $_POST['firstname'];
@@ -74,33 +74,33 @@
 
 				    try {
 				        //Server settings
-				        // $mail->isSMTP();                                     
-				        // $mail->Host ='email-smtp.us-east-1.amazonaws.com';						                     
-				        // $mail->SMTPAuth = true;                               
-				        // $mail->Username = 'AKIA5VEIXVYZ7WCS6SEY';     
-						// $mail->Password = 'BHA/c8/k/EFXNdsp0ed10uwYZDHxhc2A3/jMvfOywsb7';						            
-						// $mail->sender = 'nehadeshpande1995@gmail.com';
-				     	// $mail->senderName = 'Neha Deshpande';
-					    // $mail->recipient = $email;                           
-				        // $mail->SMTPSecure = 'tls';                           
-				        // $mail->Port = 587;                                   
-						// $mail->SMTPOptions = array(
-						// 	'ssl' => array(
-						// 	'verify_peer' => false,
-						// 	'verify_peer_name' => false,
-						// 	'allow_self_signed' => true
-						// 	)
-						// 	);
-				        // $mail->setFrom($mail->sender,$mail->senderName);
+				        $mail->isSMTP();                                     
+				        $mail->Host ='email-smtp.us-east-1.amazonaws.com';						                     
+				        $mail->SMTPAuth = true;                               
+				        $mail->Username = 'AKIA5VEIXVYZ7WCS6SEY';     
+						$mail->Password = 'BHA/c8/k/EFXNdsp0ed10uwYZDHxhc2A3/jMvfOywsb7';						            
+						$mail->sender = 'nehadeshpande1995@gmail.com';
+				     	$mail->senderName = 'Neha Deshpande';
+					    $mail->recipient = $email;                           
+				        $mail->SMTPSecure = 'tls';                           
+				        $mail->Port = 587;                                   
+						$mail->SMTPOptions = array(
+							'ssl' => array(
+							'verify_peer' => false,
+							'verify_peer_name' => false,
+							'allow_self_signed' => true
+							)
+							);
+				        $mail->setFrom($mail->sender,$mail->senderName);
 				        
-				        // //Recipients
-				        // $mail->addAddress($mail->recipient);              
-				        // //Content
-				        // $mail->isHTML(true);                                  
-				        // $mail->Subject = 'ECommerce Site Sign Up';
-				        // $mail->Body    = $message;
+				        //Recipients
+				        $mail->addAddress($mail->recipient);              
+				        //Content
+				        $mail->isHTML(true);                                  
+				        $mail->Subject = 'ECommerce Site Sign Up';
+				        $mail->Body    = $message;
 
-				        // $mail->send();
+				        $mail->send();
 
 						sendEmail($email);
 				        unset($_SESSION['firstname']);
