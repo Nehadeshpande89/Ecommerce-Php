d7b6d5fc5d277224186298438a4a790f4e6f06eb
nehadeshpande89 <nehadeshpande1995@gmail.com>
2020-12-19 15:07:51 +0000

    changes

--- register.php
+++ register.php
@@ -73,16 +73,22 @@
 				    try {
 				        //Server settings
 				        $mail->isSMTP();                                     
-				        $mail->Host = 'email-smtp.us-east-1.amazonaws.com';						                     
+				        $mail->Host ='email-smtp.us-east-1.amazonaws.com';						                     
 				        $mail->SMTPAuth = true;                               
-				        $mail->Username = 'AKIA5VEIXVYZ3HQ63BX5';     
-						$mail->Password = 'BEBPJlj0IX64IAd4tcH7LvarJ8j6wDbaw58gVJyp4nok';						            
+				        $mail->Username = 'AKIA5VEIXVYZ7WCS6SEY';     
+						$mail->Password = 'BHA/c8/k/EFXNdsp0ed10uwYZDHxhc2A3/jMvfOywsb7';						            
 						$mail->sender = 'nehadeshpande1995@gmail.com';
 				     	$mail->senderName = 'Neha Deshpande';
 					    $mail->recipient = $email;                           
 				        $mail->SMTPSecure = 'tls';                           
-				        $mail->Port = 465;                                   
-
+				        $mail->Port = 2465;                                   
+						$mail->SMTPOptions = array(
+							'ssl' => array(
+							'verify_peer' => false,
+							'verify_peer_name' => false,
+							'allow_self_signed' => true
+							)
+							);
 				        $mail->setFrom($mail->sender,$mail->senderName);
 				        
 				        //Recipients
