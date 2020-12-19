a10611a6c88fa9d4ea5b6cfe293af24f67396711
nehadeshpande89 <nehadeshpande1995@gmail.com>
2020-12-19 17:08:01 +0000

    changes

--- includes/Email_library.php
+++ includes/Email_library.php
@@ -5,11 +5,14 @@ use PHPMailer\PHPMailer\Exception;
 
 
 function sendEmail($email){
-		    		require 'vendor/autoload.php';
+
+
+	                echo "Cominge ere ";
+			    		require 'vendor/autoload.php';
 
 					    $mail = new PHPMailer(true);       
 
-				                                            
+				        $mail->isSMTP();                                     
 				        $mail->Host ='email-smtp.us-east-1.amazonaws.com';						                     
 				        $mail->SMTPAuth = true;                               
 				        $mail->Username = 'AKIA5VEIXVYZ7WCS6SEY';     
