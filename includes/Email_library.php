bf87f60d9a03684b4f9fb3a5e5f81834f8092d8d
nehadeshpande89 <nehadeshpande1995@gmail.com>
2020-12-19 16:56:23 +0000

    changes

--- includes/Email_library.php
+++ includes/Email_library.php
@@ -2,9 +2,7 @@
 //Load phpmailer
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+
 
 function sendEmail($email){
 		    		require 'vendor/autoload.php';
--- register.php
+++ register.php
@@ -1,9 +1,7 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+	
 
 	include 'includes/session.php';
 	include 'includes/Email_library.php';
