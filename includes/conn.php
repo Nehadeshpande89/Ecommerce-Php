46d9d9b2347df192262d2f05c82576cc718e0cc9
nehadeshpande89 <nehadeshpande1995@gmail.com>
2020-12-19 01:45:50 +0000

    changes

--- includes/conn.php
+++ includes/conn.php
@@ -2,7 +2,7 @@
 
 Class Database{
  
-	private $server = "ecommdb.cp0j8kjw11ud.us-east-1.rds.amazonaws.com;dbname=ecommdb";
+	private $server = "ecommdb.cp0j8kjw11ud.us-east-1.rds.amazonaws.com;dbname=ecomm";
 	private $username = "neha";
 	private $password = "12345678";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
