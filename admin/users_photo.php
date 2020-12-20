9de46741708d94fa2c33d754596eeef069db93e8
nehadeshpande89 <nehadeshpande1995@gmail.com>
2020-12-20 02:18:38 +0000

    changes

--- admin/users_photo.php
+++ admin/users_photo.php
@@ -1,34 +1,11 @@
 <?php
 	include 'includes/session.php';
-	use Aws\S3\S3Client;
-	use Aws\S3\Exception\S3Exception;
 
 	if(isset($_POST['upload'])){
 		$id = $_POST['id'];
 		$filename = $_FILES['photo']['name'];
-		$temp_file_location = $_FILES['photo']['tmp_name']; 
-
 		if(!empty($filename)){
-			move_uploaded_file($_FILES['photo']['tmp_name'], '../images/'.$filename);
-			require 'vendor/autoload.php';
-		
-
-			$s3 = new Aws\S3\S3Client([
-				'region'  => 'us-east-1',
-				'version' => 'latest',
-				'credentials' => [
-					'key'    => "AKIA5VEIXVYZ5SKBNQ55-",
-					'secret' => "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",
-				]
-			]);		
-
-			$result = $s3->putObject([
-				'Bucket' => 's3-ecomm',
-				'Key'    => $file_name,
-				'SourceFile' => $temp_file_location			
-			]);
-
-		var_dump($result);	
+			move_uploaded_file($_FILES['photo']['tmp_name'], '../images/'.$filename);	
 		}
 		
 		$conn = $pdo->open();
