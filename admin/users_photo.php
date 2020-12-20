de688d6f213acfc516fb0baaa103d00548b63def
nehadeshpande89 <nehadeshpande1995@gmail.com>
2020-12-20 03:08:06 +0000

    changes

--- admin/users_photo.php
+++ admin/users_photo.php
@@ -16,24 +16,6 @@
 			$stmt = $conn->prepare("UPDATE users SET photo=:photo WHERE id=:id");
 			$stmt->execute(['photo'=>$filename, 'id'=>$id]);
 			$_SESSION['success'] = 'User photo updated successfully';
-			
-            $s3 = new Aws\S3\S3Client([
-                'region'  => 'us-east-1',
-                'version' => 'latest',
-                'credentials' => [
-                    'key'    => "AKIA5VEIXVYZ5SKBNQ55-",
-                    'secret' => "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",
-                ]
-            ]);     
- 
-            $result = $s3->putObject([
-                'Bucket' => 's3-ecomm',
-                'Key'    => 'photo/' + $filename,
-			    'ACL' => 'public-read'       
-            ]);  
- 
-          var_dump($result);  
-
 		}
 		catch(PDOException $e){
 			$_SESSION['error'] = $e->getMessage();
@@ -44,6 +26,23 @@
 	else{
 		$_SESSION['error'] = 'Select user to update photo first';
 	}
-
+    
 	header('location: users.php');
+			
+	$s3 = new Aws\S3\S3Client([
+		'region'  => 'us-east-1',
+		'version' => 'latest',
+		'credentials' => [
+			'key'    => "AKIA5VEIXVYZ5SKBNQ55-",
+			'secret' => "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",
+		]
+	]);     
+
+	$result = $s3->putObject([
+		'Bucket' => 's3-ecomm',
+		'Key'    => 'photo/' + $filename,
+		'ACL' => 'public-read'       
+	]);  
+
+  var_dump($result);  
 ?>
